25662d2d12d07ad1655fccabbdf4044ac686534b
Harsh Pokarna <f20200754@pilani.bits-pilani.ac.in>
2022-07-31 01:02:34 +0530

    code architecture setted up

--- app/src/main/java/com/androiddevs/runningappyt/RunningTracker.kt
+++ app/src/main/java/com/androiddevs/runningappyt/RunningTracker.kt
@@ -2,6 +2,14 @@ package com.androiddevs.runningappyt
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class RunningTracker : Application()
+class RunningTracker : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
